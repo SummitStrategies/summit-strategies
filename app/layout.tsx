cbd799d30491341dc19bba87dda70294a3ee386c
Harsh <thakoreharsh58@gmail.com>
2026-04-09 17:14:44 +1000

    Add Google Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,6 +84,9 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "https://summitstrategies.au",
   },
+  verification: {
+    google: "mrLuXyohwqliwFSNNQdhpaFpqKd3H7NE5q_Fxdlq_Zw",
+  },
 };
 
 export default function RootLayout({
